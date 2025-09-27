c0b426920772785c713d471c81b79b561a794829
Mason Harbord <pugsiamese@gmail.com>
2025-09-27 18:40:51 -0500

    defined the map array better

--- backend/GameState.py
+++ backend/GameState.py
@@ -2,7 +2,7 @@ import Constants
 import math
 
 class GameState:
-    def __init__(self, team_color: str) -> None:
+    def __init__(self, team_color: str, map_width: int, map_height: int) -> None:
         self.turns_progressed = 0
         self.victory = None
         if team_color in ['r','b']:
@@ -13,9 +13,10 @@ class GameState:
         self.money = Constants.INITIAL_MONEY
 
         # Map specifications
-        self.map_width = None
-        self.map_height = None
-        self.entity_grid = [] # This is going to be a 2D array, needs input from map_width and map_height
+        self.map_width = map_width
+        self.map_height = map_height
+        # This grid is just "0" for now, but the width and height are set
+        self.entity_grid = [ [0 for i in range(map_height)] , [0 for i in range(map_width)]]
         self.tile_grid = [] # List of strings
 
         self.player_base_r = None # call PlayerBase() to define more, PlayerBase.py still needs updated
