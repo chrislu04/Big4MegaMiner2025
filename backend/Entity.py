57d8dece4e3e7625b20f2364ce2611a4d8d59142
Mason Harbord <pugsiamese@gmail.com>
2025-09-27 17:54:52 -0500

    created Entity main class

--- backend/Entity.py
+++ backend/Entity.py
@@ -1,1 +1,9 @@
+import Constants
+
 # Probably going to store health and position
+
+class Entity:
+    def __int__(self, health: int, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+        self.health = health
