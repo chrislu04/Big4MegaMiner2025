cb52b802c1f2abbf72e733b4ddf510bf46c6bc54
Eliot <eaknxp@umsystem.edu>
2025-09-30 18:33:54 -0500

    tower activations

--- backend/UpdateTowers.py
+++ backend/UpdateTowers.py
@@ -4,7 +4,7 @@ from House import House
 from Cannon import Cannon
 from Minigun import Minigun
 from Crossbow import Crossbow
-from Constants import HOUSE_MAX_COOLDOWN, CANNON_MAX_COOLDOWN, MINIGUN_MAX_COOLDOWN, CROSSBOW_MAX_COOLDOWN
+import Constants
 
 def update_towers(game_state: GameState):
     towers = game_state.towers
@@ -18,33 +18,50 @@ def update_towers(game_state: GameState):
         if isinstance(tower, House):
             if current_cooldown > 0:
                 current_cooldown -= 1
-                if current_team == 'r':
-                    game_state.money_r += 12
-                else:
-                    game_state.money_b += 12
             else:
-                current_cooldown = HOUSE_MAX_COOLDOWN
+                if current_team == 'r':
+                    game_state.money_r += Constants.HOUSE_MONEY_PRODUCED
+                else:
+                    game_state.money_b += Constants.HOUSE_MONEY_PRODUCED
+                tower.current_cooldown = Constants.HOUSE_MAX_COOLDOWN
 
         ## Cannon Tower activation
-        if isinstance(tower, Cannon):
+        elif isinstance(tower, Cannon):
             if current_cooldown > 0:
                 current_cooldown -= 1
-                # Create projectile that damages enemy mercs
             else:
-                current_cooldown = CANNON_MAX_COOLDOWN
-
-        ## Minigun Tower activation
-        if isinstance(tower, Minigun):
-            if current_cooldown > 0:
-                current_cooldown -= 1
-                # Create projectile that damages enemy mercs
-            else:
-                current_cooldown = MINIGUN_MAX_COOLDOWN
+                enemies_in_range = find_all_enemies_in_range(game_state, tower.x, tower.y, current_team, Constants.CANNON_RANGE)
+                if len(enemies_in_range) > 0:
+                    # Attack the highest-priority enemy
+                    highest_priority = enemies_in_range[0]
+                    highest_priority.health -= Constants.CANNON_DAMAGE
+                    tower.current_cooldown = Constants.CANNON_MAX_COOLDOWN
 
         ## Crossbow Tower activation
-        if isinstance(tower, Crossbow):
+        elif isinstance(tower, Crossbow):
             if current_cooldown > 0:
                 current_cooldown -= 1
-                # Create projectile that damages enemy mercs
             else:
-                current_cooldown = CROSSBOW_MAX_COOLDOWN
+                enemies_in_range = find_all_enemies_in_range(game_state, tower.x, tower.y, current_team, Constants.CROSSBOW_RANGE)
+                if len(enemies_in_range) > 0:
+                    # Attack the highest-priority enemy
+                    highest_priority = enemies_in_range[0]
+                    highest_priority.health -= Constants.CROSSBOW_DAMAGE
+                    tower.current_cooldown = Constants.CROSSBOW_MAX_COOLDOWN
+        
+        ## Minigun Tower activation
+        elif isinstance(tower, Minigun):
+            if current_cooldown > 0:
+                current_cooldown -= 1
+            else:
+                enemies_in_range = find_all_enemies_in_range(game_state, tower.x, tower.y, current_team, Constants.MINIGUN_RANGE)
+                if len(enemies_in_range) > 0:
+                    # Attack all enemies in range
+                    for enemy in enemies_in_range:
+                        enemy.health -= Constants.MINIGUN_DAMAGE
+                    tower.current_cooldown = Constants.MINIGUN_MAX_COOLDOWN
+
+# Ordered by priority of attack
+# TODO: figure out what this priority is, and put that in the rules
+def find_all_enemies_in_range(game_state: GameState, x: int, y: int, team_color: str, range: int) -> list:
+    pass # TODO
