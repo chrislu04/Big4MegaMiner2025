7568822687470ecc737bef558ce289ee1a957605
mjjm4d <threehalves1@gmail.com>
2025-10-04 17:09:42 -0500

    Implement demon logic

--- backend/Demon.py
+++ backend/Demon.py
@@ -1,9 +1,53 @@
 import Constants
 from Entity import Entity
+from GameState import GameState
+import Utils
 
 class Demon:
-    def __init__(self, x: int, y: int, target_team: str) -> None:
+    def __init__(self, x: int, y: int, target_team: str, state: str) -> None:
         self.hp = Constants.DEMON_INITIAL_HP
         self.x = x
         self.y = y
         self.target_team = target_team
+        self.state = state
+
+    # Helper function do find what path this merc is on. 
+    # TODO: Store current path and path index in merc object
+    def get_current_path(self, game_state: GameState):
+        current_path = []
+        possible_paths = [game_state.mercenary_path_down, 
+                    game_state.mercenary_path_left, game_state.mercenary_path_right,
+                    game_state.mercenary_path_up]
+        # Assumes no overlapping paths. If merc is on any path tile in path, we are on that path
+        for path in possible_paths:
+            if path == None: #<- in case there aren't 4 paths, it will skip iterating over a none value (which cases an error)
+                continue 
+            if (self.x, self.y) in path:
+                current_path = path
+                
+        # return current path and position along current path
+        return (current_path, current_path.index((self.x, self.y)))
+    
+    # Helper function that returns coordinates of the path tile forward or back from the merc's pos
+    def get_adjacent_path_tile(self, game_state: GameState, delta: int):
+        path_data = self.get_current_path(game_state)
+        path = path_data[0]
+        path_pos = path_data[1]
+
+        # if we are at the end of path, return last tile 
+        # otherwise, return next tiles
+        delta *= 1 if self.target_team == 'r' else -1
+        return path[Utils.clamp(path_pos + delta, 0, len(path))]
+    
+    def set_behind_waiting(self, game_state: GameState):
+        behind_pos = self.get_adjacent_path_tile(game_state, -1)
+        behind_entity = game_state.entity_grid[behind_pos[0]][behind_pos[1]]
+        # base case: we are in the first tile in our path, do not recurse
+        if self.x == behind_pos[0] and self.y == behind_pos[1]:
+            return
+        # base case: entity in behind pos does not contain merc
+        elif type(behind_entity) != Demon:
+            return
+        else:
+            behind_entity.state = 'waiting'
+            behind_entity.set_behind_waiting(game_state)
--- backend/UpdateDemons.py
+++ backend/UpdateDemons.py
@@ -1,4 +1,87 @@
+from typing import List
 from GameState import GameState
+from Demon import Demon
+from PlayerBase import PlayerBase
+from Mercenary import Mercenary
+from Entity import Entity
+import Constants
+
+# NOTE: Function logic has been copy-pasted from UpdateMercenaries.py.
+# This makes the code messier but may be useful if demons & mercs need independant tweaking
 
 def update_demons(game_state: GameState):
-    pass # TODO
+    # Determine all demon states
+    moving: List[Demon] = []
+    fighting: List[Demon] = []
+    waiting: List[Demon] = []
+    set_all_demon_states(game_state, game_state.mercs, moving, fighting, waiting)
+
+    # Move all mercs in moving state
+    move_all_demons(game_state, moving)
+
+    # Apply combat effects for all mercs in fighting state
+    for merc in fighting:
+        do_demon_combat_single(game_state, merc)
+
+# Side effect: All merc objects will have their state set appropiately
+# Side effect: moving, fighting and waiting lists will be populated
+def set_all_demon_states(game_state: GameState, demons: List[Demon], 
+                        moving: List[Demon],
+                        fighting: List[Demon],
+                        waiting: List[Demon]):
+    for demon in demons:
+        next_tile1 = demon.get_adjacent_path_tile(game_state, 1)
+        next_tile2 = demon.get_adjacent_path_tile(game_state, 2)
+        blocking_entity1 = game_state.entity_grid[next_tile1[0]][next_tile1[1]]
+        blocking_entity2 = game_state.entity_grid[next_tile2[0]][next_tile2[1]]
+
+        # fighting if rival merc or demon or player base is within 1 space
+        if (type(blocking_entity1) == type(Mercenary) or 
+            type(blocking_entity1) == type(PlayerBase) or
+            (type(blocking_entity1) == type(Demon) and blocking_entity1.target_team != demon.team)):
+            demon.state = 'fighting'
+            # set all demons behind us to waiting
+            demon.set_behind_waiting(game_state)
+        # fighting if enemy is within 2 spaces and merc within 1 space is not ally
+        elif (type(blocking_entity1) == None and
+            type(blocking_entity2) == type(Demon) or 
+            type(blocking_entity2) == type(PlayerBase) or
+            (type(blocking_entity2) == type(Mercenary) and blocking_entity2.team != demon.team)):
+            demon.state = 'fighting'
+            # set all demons behind us to waiting
+            demon.set_behind_waiting(game_state)
+        # if not waiting or fighting, then moving
+        elif demon.state != 'waiting':
+            demon.state = 'moving'
+        
+        # add to correct list
+        if demon.state == 'fighting': fighting.append(demon)
+        if demon.state == 'waiting': waiting.append(demon)
+        if demon.state == 'moving': moving.append(demon)
+
+def move_all_demons(game_state: GameState, demons: List[Demon]):
+    # remove moving demons
+    for demon in demons:
+        game_state.entity_grid[demon.x][demon.y] = None
+
+    # set new position
+    for demon in demons:
+        new_pos = demon.get_adjacent_path_tile(game_state, 1)
+        demon.x = new_pos[0]
+        demon.y = new_pos[1]
+
+    # add moving demons back
+    for demon in demons:
+        game_state.entity_grid[demon.x][demon.y] = demon
+
+def do_demon_combat_single(game_state: GameState, demon: Demon):
+    next_tile1 = demon.get_adjacent_path_tile(game_state, 1)
+    next_tile2 = demon.get_adjacent_path_tile(game_state, 2)
+    target1: Entity = game_state.entity_grid[next_tile1[0], next_tile1[1]]
+    target2: Entity = game_state.entity_grid[next_tile2[0], next_tile2[1]]
+    
+    # if tile 1 space in front is empty, we are contesting space with enemy 2 spaces in front 
+    if (target1 != None):
+        target1.health -= Constants.DEMON_ATTACK_POWER
+    else:
+        target2.health -= Constants.DEMON_ATTACK_POWER
