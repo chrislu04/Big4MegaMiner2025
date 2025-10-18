13063eeb2f72a809ce0c250a07b187d20d900621
mjjm4d <threehalves1@gmail.com>
2025-10-18 17:12:21 -0500

    Make PlayerBase inherit from Entity

--- backend/PlayerBase.py
+++ backend/PlayerBase.py
@@ -1,10 +1,9 @@
 import Constants
+from Entity import Entity
 
-class PlayerBase():
-    def __init__(self, x: int, y: int, team_color: str) -> None:
-        self.hp = Constants.PLAYER_BASE_INITIAL_HP
-        self.x = x
-        self.y = y
+class PlayerBase(Entity):
+    def __init__(self, x: int, y: int, team_color: str, value=0, attack=0) -> None:
+        super().__init__(Constants.PLAYER_BASE_INITIAL_HP, value, attack, x, y)
         self.mercenary_queued_up : int = 0
         self.mercenary_queued_down : int = 0
         self.mercenary_queued_left : int = 0
